55867e6e439b288fe9e9d55329324bdced483be0
Weibo Qiu <cucqiuweibo@gmail.com>
2018-10-07 11:20:55 +0200

    delete old files

--- js/models/wallet-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const walletSchema = mongoose.Schema({
-    name: String,
-    hash: String,
-    quantity: Number
-});
-
-module.exports = mongoose.model('Wallet', walletSchema);
